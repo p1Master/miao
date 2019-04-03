b650286874d948822cc2d4555d3285c182c33dbf
PiPln <nserkaka@gmail.com>
2019-04-03 18:01:42 +0800

    1

---
+++ src/views/Cinema/index.vue
@@ -0,0 +1,39 @@
+<template>
+    <div id="main">
+        <Header title="喵喵影院"/>
+        <div id="content">
+            <div class="cinema_menu">
+                <div class="city_switch">
+                    全城 <i class="iconfont icon-lower-triangle"></i>
+                </div>
+                <div class="brand_switch">
+                    品牌 <i class="iconfont icon-lower-triangle"></i>
+                </div>
+                <div class="feature_switch">
+                    特色 <i class="iconfont icon-lower-triangle"></i>
+                </div>
+            </div>
+            <Cilist />
+        </div>
+        <TabBar />
+    </div>
+</template>
+
+<script>
+    import Header from '@/components/header';
+    import TabBar from '@/components/tabbar';
+    import Cilist from '@/components/cilist';
+
+    export default {
+        name: "Ciname",
+        components:{
+            Header,
+            TabBar,
+            Cilist
+        }
+    }
+</script>
+
+<style scoped>
+#content .cinema_menu{ width: 100%; height: 45px; border-bottom:1px solid #e6e6e6; display: flex; justify-content:space-around; align-items:center; background:white;}
+</style>
---
+++ src/views/Mine/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="main">
+        <Header title="我的喵喵" />
+        <div id="content">
+            <Login />
+        </div>
+        <TabBar />
+    </div>
+</template>
+
+<script>
+    import Header from '@/components/header';
+    import TabBar from '@/components/tabbar';
+    import Login from '@/components/login';
+
+    export default {
+        name: "Mine",
+        components:{
+            Header,
+            TabBar,
+            Login
+        }
+    }
+</script>
+
+<style>
+
+</style>
